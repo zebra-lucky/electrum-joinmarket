aaeb4cbaf4f53d1d698328673207537efdfaa7b4
zebra-lucky <zebra.lucky@gmail.com>
2024-10-27 14:01:13 +0200

    update to latest base code (no changes to plugin)

--- electrum/plugins/joinmarket/__init__.py
+++ electrum/plugins/joinmarket/__init__.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 
 # based: on https://github.com/JoinMarket-Org/joinmarket-clientserver
+# commit a0761bf18b9de94fbe4fdc53e05c368b050971fb
+# Author: merge-script <kristaps@blogiem.lv>
+# Date:   Wed Oct 23 22:20:51 2024 +0300
+
+# prev based: on https://github.com/JoinMarket-Org/joinmarket-clientserver
 # commit 75e0bc40ffc37604c128922da11669794e76bba1
 # Author: merge-script <kristaps@blogiem.lv>
 # Date:   Fri Aug 9 13:06:08 2024 +0300
 
-# prev based:
-# commit f83c4208a4507f3f31ab57996636722d7db78fb2
-# Author: Kristaps Kaupe <kristaps@blogiem.lv>
-# Date:   Wed Feb 14 13:49:36 2024 +0200
-
-# additional tests requirement: unittest-parametrize==1.4.0
+# additional tests requirements: unittest-parametrize==1.4.0
 
 fullname = 'JoinMarket'
 description = ' '.join([
